01b90a379bd36a60803c77c9b18f40953a8c570d
ranisumbul7 <ranisumbul7@gmail.com>
2025-05-12 20:32:14 +0530

    ArrayElement.java

---
+++ DSAcode/ArayElement.java
@@ -0,0 +1,13 @@
+//print all array elements 
+public class PrintArray {
+    public static void main(String[] args) {
+        // Step 1: Declare and initialize the array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Step 2: Loop through the array using for loop
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + " = " + numbers[i]);
+        }
+    }
+}
+
