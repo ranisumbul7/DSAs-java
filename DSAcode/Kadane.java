2557a73240cfbf6985ba27cc41d5a623fdc13642
ranisumbul7 <ranisumbul7@gmail.com>
2025-05-18 13:01:13 +0530

    Kadane.java

---
+++ DSAcode/Kadane.java
@@ -0,0 +1,18 @@
+public class Kadane {
+    public static int maxSubArraySum(int[] arr) {
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum is: " + maxSubArraySum(arr));
+    }
+}
