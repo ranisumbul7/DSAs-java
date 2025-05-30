d9c6fa2fc5b13179f62eacaf7b84d8735857a66f
ranisumbul7 <ranisumbul7@gmail.com>
2025-05-30 23:09:10 +0530

    Create CountInversion.java

---
+++ DSAcode/CountInversion.java
@@ -0,0 +1,21 @@
+public class CountInversions {
+    public static int countInversions(int[] arr) {
+        int count = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        System.out.println("Inversion Count: " + countInversions(arr));
+    }
+}
