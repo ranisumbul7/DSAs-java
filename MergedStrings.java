39fd6e75d3ab654a83b19dff573a520fde003b1b
ranisumbul7 <ranisumbul7@gmail.com>
2025-06-02 23:11:06 +0530

    Create MergedStrings.java

---
+++ MergedStrings.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        String merged = str1 + str2;
+        System.out.println("merged string: " + merged);
+    }
+}
