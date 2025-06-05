efb7bfb91113abf1005bb21834b3c87817ad15b8
ranisumbul7 <ranisumbul7@gmail.com>
2025-06-05 12:15:30 +0530

    Create MinMax.java

---
+++ DSAcode/MinMax.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[] args){
+       int a = 100;
+       int b = 40;
+       int max;
+       
+       if(a>b){
+           max = a;
+       }else{
+           max = b;
+       }
+       System.out.println("The maximum of " + a + " and " + b  + " is: " + max);
+    }
+}
